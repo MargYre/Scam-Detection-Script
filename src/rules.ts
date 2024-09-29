7371a1a4b6930f87cc7e85c590e1dad93e30865c
margaux <margaux.tarayre@gmail.com>
2024-09-29 11:04:20 +0200

    Add alphaRateRule and numberRateRule in rules.ts

--- src/rules.ts
+++ src/rules.ts
@@ -10,3 +10,15 @@ export const firstnameLengthRule = (contacts: Contacts): boolean => {
 export const lastnameLengthRule = (contacts: Contacts): boolean => {
     return contacts.lastName.length > 2;
 };
+
+export const alphaRateRule = (contacts: Contacts): boolean => {
+    const emailPart = contacts.email.split('@')[0];
+    const alphanumericCount = emailPart.replace(/[^a-zA-Z0-9]/g, "").length;
+    return (alphanumericCount / emailPart.length) > 0.7;
+};
+
+export const numberRateRule = (contacts: Contacts): boolean => {
+    const emailPart = contacts.email.split('@')[0];
+    const numericCount = emailPart.replace(/[^0-9]/g, "").length;
+    return (numericCount / emailPart.length) < 0.3;
+  };
