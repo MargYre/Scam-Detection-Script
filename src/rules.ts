e1c585d6043c86e715a33f63b07ed670cbc7a441
margaux <margaux.tarayre@gmail.com>
2024-09-29 12:11:45 +0200

    Add priceQuotationRateRule and registerNumberBlocklistRule in rules.ts

--- src/rules.ts
+++ src/rules.ts
@@ -21,4 +21,13 @@ export const numberRateRule = (contacts: Contacts): boolean => {
     const emailPart = contacts.email.split('@')[0];
     const numericCount = emailPart.replace(/[^0-9]/g, "").length;
     return (numericCount / emailPart.length) < 0.3;
-  };
+};
+  
+export const priceQuotationRateRule = async (vehicle: Vehicle, price: number): Promise<boolean> => {
+    const quotation = await QuotationService(vehicle);
+    return Math.abs(quotation - price) / quotation <= 0.2;
+};
+
+export const registerNumberBlocklistRule = async (vehicle: Vehicle): Promise<boolean> => {
+    return !(await BlocklistService(vehicle.registerNumber));
+};
